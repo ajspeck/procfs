ceada26f535e37662111958a369ad491eef8563f
ajspeck <15779151+ajspeck@users.noreply.github.com>
2020-01-20 09:58:30 -0500

    Update proc.go

--- proc.go
+++ proc.go
@@ -21,8 +21,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/prometheus/procfs/internal/fs"
-	"github.com/prometheus/procfs/internal/util"
+	"github.com/ajspeck/procfs/internal/fs"
+	"github.com/ajspeck/procfs/internal/util"
 )
 
 // Proc provides information about a running process.
